327991fd9f48b46e3c3b87bb78954417e9e01545
alma-claudio <50150616+alma-claudio@users.noreply.github.com>
2021-10-23 19:08:45 -0500

    Update inserts.sql

--- DBA/inserts.sql
+++ DBA/inserts.sql
@@ -344,5 +344,17 @@ insert into contracargo(id_transaccion,id_tipo_contracargo)
 values (53,2);
 insert into contracargo(id_transaccion,id_tipo_contracargo)
 values (54,3);
+
+-------------------------------Inserts de estado ----------------------
+------------------------------------------------------------------------
+insert into estado(id_estado,estado)
+values(1,'Aguascalientes'),(2,'Baja California'),(3,'Baja California Sur')
+,(4,'Campeche'),(5,'Coahuila de Zaragoza'),(6,'Colima'),(7,'Chiapas'),
+(8,'Chihuahua'),(9,'CDMX'),(10,'Durango'),(11,'Guanajuato'),(12,'Guerrero'),
+(13,'Hidalgo'),(14,'Jalisco'),(15,'México'),(16,'Michoacán de Ocampo'),
+(17,'Morelos'),(18,'Nayarit'),(19,'Nuevo Leon'),(20,'Oaxaca'),(21,'Puebla'),
+(22,'Querétaro'),(23,'Quintana Roo'),(24,'San luis Potosi'),(25,'Sinaloa'),
+(26,'Sonora'),(27,'Tabasco'),(28,'Tamaulipas'),(29,'Tlaxcala'),
+(30,'Veracruz'),(31,'Yucata'),(32,'Zacatecas');
 insert into contracargo(id_transaccion,id_tipo_contracargo)
 values (55,1);
